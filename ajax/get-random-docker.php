e49093fe1f8ae3904fc62810bda3ef4c88392d26
* <lucasduartedeveloper@gmail.com>
2023-07-11 00:06:06 -0300

    first commit

--- ajax/get-random-docker.php
+++ ajax/get-random-docker.php
@@ -5,9 +5,10 @@ $z =  rand(0, 1000);
 $time = time();
 
 $array = array(
-   “x” => $x,
-   “y” => $y,
-   “z” => $z,
+   "x" => $x,
+   "y" => $y,
+   "z" => $z,
+   "time" => $time
 );
 echo json_encode($array);
 ?>
